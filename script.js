ae27c9a5f1c00e2050a786396474fb173ac6eb3a
Julio Caro <jcaro@matco.com.mx>
2022-02-20 15:43:23 -0700

    Todo bien... cambios necesarios para el ejercicio

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    amount = interest;
+
+
+    var resultstr = "If you deposit " + principal + ", \<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year +"\<br\>";
+    //alert(resultstr);
+    document.getElementById("result").innerHTML=resultstr;
     
 }
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
         
