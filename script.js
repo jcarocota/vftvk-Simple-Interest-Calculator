4a5fb840080021cf86556a5ef4583f3ca7061a0b
Julio Caro <jcaro@matco.com.mx>
2022-02-20 16:01:11 -0700

    Otro pequeÃo cambio. Se agregÃ³ focus en error.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function compute()
 {
     
     var principal = document.getElementById("principal").value;
-
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
@@ -16,6 +15,7 @@ function compute()
     if(principal <= 0) {
         alert("You must enter a positive amount");
         var resultstr = "Not valid input";
+        document.getElementById("principal").focus();
     } else {
         var resultstr = "If you deposit " + principal + ", \<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year +"\<br\>";
     }
